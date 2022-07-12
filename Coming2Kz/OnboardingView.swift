c5c451234478fc08cb55eab50a372b7aee9c2b98
Tony Kara <a.eniola@vlife.kz>
2022-07-13 02:01:12 +0600

    feat: add button to onboarding view

--- Coming2Kz/OnboardingView.swift
+++ Coming2Kz/OnboardingView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button")
     var body: some View {
-      RiveViewModel(fileName: "shapes").view()
-        .ignoresSafeArea()
-        .blur(radius: 30)
-        .background(
-         Image("Spline")
-          .blur(radius: 50)
-          .offset(x:200, y: 100)
+        ZStack {
+            RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+             Image("Spline")
+              .blur(radius: 50)
+              .offset(x:200, y: 100)
         )
+            
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                Label("Start Chat", systemImage: "arrow.forward")
+                    .offset(x: 4, y: 4)
+                    .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    button.play(animationName: "active")
+                }
+        }
+        
+    
+        
     }
 }
 
