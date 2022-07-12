c559d00e1f54a59400e9bf09b9daf32779848fff
Eniola Anthony <eniolaanthony@gmail.com>
2022-07-12 17:53:46 +0600

    Initial Commit

---
+++ Coming2Kz/Coming2KzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coming2KzApp.swift
+//  Coming2Kz
+//
+//  Created by User on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Coming2KzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
