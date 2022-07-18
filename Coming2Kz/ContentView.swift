ff195e7a0f5f83ded40e58fd6999f1c5b9c09631
Eniola Anthony <eniolaanthony@gmail.com>
2022-07-18 19:52:21 +0600

    feat: add Menu Animation

--- Coming2Kz/ContentView.swift
+++ Coming2Kz/ContentView.swift
@@ -6,11 +6,43 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @AppStorage("selectedTab") var selectedTab: Tab = .chat
+  @State var isOpen = true
+  var button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
+  
+  
+  var body: some View {
+      ZStack {
+        switch selectedTab {
+        case .chat:
+          Text("Hello, world!")
+        case .search:
+          Text("Hello, world!")
+        case .timer:
+          Text("Hello, world!")
+        case .bell:
+          Text("Hello, world!")
+        case .user:
+          Text("Hello, world!")
+        }
+        
+        button.view()
+          .frame(width: 44, height: 44)
+          .mask(Circle())
+          .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+          .frame(maxWidth:.infinity, maxHeight:.infinity, alignment: .topLeading)
+          .padding()
+          .onTapGesture {
+           button.setInput("isOpen", value: isOpen)
+            isOpen.toggle()
+            print(isOpen)
+          }
+        
+        TabBar()
+      }
     }
 }
 
--- Coming2Kz/TabBar.swift
+++ Coming2Kz/TabBar.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import RiveRuntime
 
 struct TabBar: View {
+  @AppStorage("selectedTab") var selectedTab: Tab = .chat
   let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
   
   var body: some View {
@@ -22,9 +23,24 @@ struct TabBar: View {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
               item.icon.setInput("active", value: false)
             }
+            withAnimation {
+              selectedTab = item.tab
+            }
           } label: {
             item.icon.view()
               .frame(height: 36)
+              .opacity(selectedTab == item.tab ? 1 : 0.5)
+              .background(
+                VStack {
+                  RoundedRectangle(cornerRadius: 2)
+                 //   .fill(.pink) change the color with this from the accent color
+                    .frame(width: selectedTab == item.tab ? 20 : 0, height:4)
+                    .offset(y: -4)
+                    .opacity(selectedTab == item.tab ? 1 : 0)
+                  Spacer()
+                }
+                    
+              )
           }
         }
       }
