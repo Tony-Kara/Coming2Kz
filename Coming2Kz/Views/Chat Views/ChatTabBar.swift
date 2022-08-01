7a9c4a973ca20ad8ac51039c2d39da1d4350d5de
Eniola Anthony <eniolaanthony@gmail.com>
2022-08-01 21:06:45 +0600

    chore: remove white lines and  unneeded comments

--- Coming2Kz/Views/Chat Views/ChatTabBar.swift
+++ Coming2Kz/Views/Chat Views/ChatTabBar.swift
@@ -40,119 +40,20 @@ struct ChatTabBar: View {
         Spacer()
         HStack(alignment: .center, spacing: 35) {
           ForEach(chatTabItems) { item in
-          Button {
-            selectedTab = item.tab
-          } label: {
-            
+            Button {
+              selectedTab = item.tab
+            } label: {
+              
               TabBarButton(selectedTab: $selectedTab, tabItem: item)
-            
-//              VStack(alignment: .center, spacing: 4) {
-//                Image(systemName: "bubble.left")
-//                  .resizable()
-//                  .scaledToFit()
-//                  .frame(width: 24, height: 24)
-//
-//                Text("Home")
-//                  .customFont(.caption2)
-//              }
-//              .background(
-//                VStack{
-//                  RoundedRectangle(cornerRadius: 2)
-//                    .fill(.blue)
-//                    .frame(width:selectedTab == .home ? 30 : 0, height: 4)
-//                    .offset(y:-4)
-//                    .opacity(1)
-//                  Spacer()
-//
-//                }
-//              )
-//
-            
-            
+            }
+            .tint(Color(item.buttonTintColour))
           }
-          .tint(Color(item.buttonTintColour))
-          }
-          
-//          Button {
-//            selectedTab = .chats
-//          } label: {
-//
-//            TabBarButton(selectedTab: $selectedTab, buttonText: "Chats", imageName: "bubble.left")
-////              VStack(alignment: .center, spacing: 4) {
-////                Image(systemName: "bubble.left")
-////                  .resizable()
-////                  .scaledToFit()
-////                  .frame(width: 24, height: 24)
-////
-////                Text("Chats")
-////                  .customFont(.caption2)
-////              }
-////              .background(
-////                VStack{
-////                  RoundedRectangle(cornerRadius: 2)
-////                    .fill(.blue)
-////                    .frame(width:selectedTab == .chats ? 30 : 0,height: 4)
-////                    .offset(y:-4)
-////                    .opacity(1)
-////                  Spacer()
-////
-////                }
-////              )
-//          }
-//          .tint(Color("icons-secondary"))
-          
-//          Button {
-//
-//          } label: {
-//            VStack(alignment: .center, spacing: 4) {
-//              Image(systemName: "plus.circle.fill")
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 32, height: 32)
-//              Text("New Chat")
-//                .customFont(.caption2)
-//            }
-//          }
-//          .tint(Color("icons-primary"))
-          
-//          Button {
-//            selectedTab = .contacts
-//          } label: {
-//
-//            TabBarButton(selectedTab: $selectedTab, buttonText: "Contacts", imageName: "person")
-//
-////              VStack(alignment: .center, spacing: 4) {
-////                Image(systemName: "person")
-////                  .resizable()
-////                  .scaledToFit()
-////                  .frame(width: 24, height: 24)
-////                Text("Contacts")
-////                  .customFont(.caption2)
-////              }
-////              .background(
-////                VStack{
-////                  RoundedRectangle(cornerRadius: 2)
-////                    .fill(.blue)
-////                    .frame(width:selectedTab == .contacts ? 30 : 0,height: 4)
-////                    .offset(y:-4)
-////                    .opacity(1)
-////                  Spacer()
-////
-////                }
-////              )
-//
-//
-//          }
-//
-//          .tint(Color("icons-secondary"))
-          
         }
         .onAppear {
           rootModel.dismissMainTab = true
-      }
+        }
       }
     }
-    
   }
 }
 
--- Coming2Kz/Views/Chat Views/TabBarButton.swift
+++ Coming2Kz/Views/Chat Views/TabBarButton.swift
@@ -10,36 +10,32 @@ import SwiftUI
 struct TabBarButton: View {
   @Binding var selectedTab : Tabs
   let tabItem: ChatTabItems
-//  var buttonText: String
-//  var imageName: String
-    var body: some View {
+  var body: some View {
+    
+    VStack(alignment: .center, spacing: 4) {
+      Image(systemName: tabItem.imageName)
+        .resizable()
+        .scaledToFit()
+        .frame(width: 24, height: 24)
       
-      VStack(alignment: .center, spacing: 4) {
-        Image(systemName: tabItem.imageName)
-          .resizable()
-          .scaledToFit()
-          .frame(width: 24, height: 24)
-
-        Text(tabItem.buttonText)
-          .customFont(.caption2)
-      }
-      .background(
-        VStack{
-          RoundedRectangle(cornerRadius: 2)
-            .fill(.blue)
-            .frame(width:selectedTab == tabItem.tab ? 30 : 0, height: 4)
-            .offset(y:-4)
-            .opacity(1)
-          Spacer()
-        
-        }
-      )
-
+      Text(tabItem.buttonText)
+        .customFont(.caption2)
     }
+    .background(
+      VStack{
+        RoundedRectangle(cornerRadius: 2)
+          .fill(.blue)
+          .frame(width:selectedTab == tabItem.tab ? 30 : 0, height: 4)
+          .offset(y:-4)
+          .opacity(1)
+        Spacer()
+      }
+    )
+  }
 }
 
 struct TabBarButton_Previews: PreviewProvider {
-    static var previews: some View {
-      TabBarButton(selectedTab: .constant(.contacts), tabItem: chatTabItems[0] )
-    }
+  static var previews: some View {
+    TabBarButton(selectedTab: .constant(.contacts), tabItem: chatTabItems[0] )
+  }
 }
